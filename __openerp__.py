631c6f4803b11252cd26d0c72cc059933fd9ee8a
Lydia Reina <reinalydia@gmail.com>
2017-12-20 17:05:22 +0100

    Solución general de errores.

--- __openerp__.py
+++ __openerp__.py
@@ -30,7 +30,7 @@
     Gestion hospitalaria.
     """,
     "init_xml": [],
-    'data': ['hospital_view.xml','medico_view.xml','cita_view.xml','ambulancia_view.xml','paciente_view.xml','tratamiento_view.xml','prueba_view.xml','aseguradora_view.xml','laboratorio_view.xml','medicamento_view.xml','workflow/cita_workflow.xml'],
+    'data': ['medico_view.xml','cita_view.xml','ambulancia_view.xml','paciente_view.xml','tratamiento_view.xml','prueba_view.xml','hospital_view.xml','aseguradora_view.xml','laboratorio_view.xml','medicamento_view.xml','workflow/cita_workflow.xml'],
     'demo_xml': ['demo_data.xml'],
     'installable': True,
     'active': False,
